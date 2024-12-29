64d2bf911f51503ea564351d2ab69a28ff4ad86d
Juanpujol674 <juanpujol674@gmail.com>
2024-12-30 00:32:25 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -45,6 +45,7 @@ git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/lucky
 git clone --depth=1 --single-branch --branch js https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic package/luci-app-unblockneteasemusic
 git clone --depth=1 --single-branch --branch main https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
 git clone https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac package/luci-app-gecoosac
+git clone --depth=1 --single-branch --branch master https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
 
 
 # 科学上网插件
