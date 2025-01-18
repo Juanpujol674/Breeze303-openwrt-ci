a96fe8c1f0bc3a557115f0e1f57b064dd7dee0fd
Juanpujol674 <juanpujol674@gmail.com>
2025-01-18 20:12:13 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -50,6 +50,8 @@ git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/lu
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 #git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
+git_sparse_clone master https://github.com/VIKINGYFY/homeproxy luci-app-homeproxy
+
 
 # Themes
 #git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
