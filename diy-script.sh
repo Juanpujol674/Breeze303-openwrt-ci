0f88711db7fb19705576c020cc3fdd6f3bb43269
Juanpujol674 <juanpujol674@gmail.com>
2025-01-25 00:12:17 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -276,17 +276,6 @@ fix_build_for_openssl() {
     fi
 }
 
-update_ath11k_fw() {
-    local makefile="$BUILD_DIR/package/firmware/ath11k-firmware/Makefile"
-    local new_mk="$BASE_PATH/patches/ath11k_fw.mk"
-
-    if [ -d "$(dirname "$makefile")" ] && [ -f "$makefile" ]; then
-        [ -f "$new_mk" ] && \rm -f "$new_mk"
-        curl -L -o "$new_mk" https://raw.githubusercontent.com/VIKINGYFY/immortalwrt/refs/heads/main/package/firmware/ath11k-firmware/Makefile
-        \mv -f "$new_mk" "$makefile"
-    fi
-}
-
 fix_mkpkg_format_invalid() {
     if [[ $BUILD_DIR =~ "imm-nss" ]]; then
         if [ -f $BUILD_DIR/feeds/small8/v2ray-geodata/Makefile ]; then
